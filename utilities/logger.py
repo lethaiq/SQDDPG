7c5b40f92a4c769e825297bcaf400ce757908d2f
“lethaiq” <leqthai.vn@gmail.com>
2021-03-18 15:45:19 -0400

    updates

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,13 +1,12 @@
 # Code referenced from https://gist.github.com/gyglim/1f8dfb1b5c82627ae3efcfbbadb9f514
 import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
 import numpy as np
 import scipy.misc
 try:
     from StringIO import StringIO  # Python 2.7
 except ImportError:
     from io import BytesIO         # Python 3.x
-
+tf.disable_v2_behavior()
 
 class Logger(object):
 
